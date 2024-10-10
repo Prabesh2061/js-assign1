0acd7f3e3afb65e5f33e46ccbad493e5e9fa98df
Prabesh2061 <aryalpravesh111@gmail.com>
2024-10-10 02:53:51 -0400

    added new button and some css changes

--- js/app.js
+++ js/app.js
@@ -1,18 +1,29 @@
 
 var synth = window.speechSynthesis;
-var speakButton = document.querySelector('button');
 var firstButton = document.querySelector('.first');
 var secondButton = document.querySelector('.second');
 var thirdButton = document.querySelector('.third');
 var fourthButton = document.querySelector('.fourth');
 var fifthButton = document.querySelector('.fifth');
 var sen = document.querySelector(".sen");
+var reset = document.querySelector(".reset");
+var story = document.querySelector(".story");
 
 var nouns = ["The turkey", "Mom", "Dad", "The dog", "My teacher", "The elephant", "The cat"];
 var verbs = ["sat on", "ate", "danced with", "saw", "doesn't like", "kissed"];
 var adjectives = ["a funny", "a scary", "a goofy", "a slimy", "a barking", "a fat"];
 var noun = ["goat", "monkey", "fish", "cow", "frog", "bug", "worm"];
 var settings = ["on the moon", "on the chair", "in my spaghetti", "in my soup", "on the grass", "in my shoes"];
+
+var buttonList = [nouns, verbs, adjectives, noun, settings];
+buttonList.forEach((e, i) => {
+	let item = document.querySelector(`.item${i+1}`);
+	e.forEach(el => {
+		let li = document.createElement("li");
+		li.textContent = el;
+		item.appendChild(li);
+	});
+});
 
 var sentence = [];
 let count = {
@@ -35,45 +46,85 @@ function countChecker(num){
 	}
 }
 
-function makeSentence(word){
-	sentence.push(word);
+function makeSentence(arr, num){
+	// method to check the if same button is pressed
+	let items = document.querySelector(`item${num}`);
+	if(sentenceChecker(arr)){
+		for (let i = 0; i < sentence.length; i++) {
+			if(arr.includes(sentence[i])){
+				sentence[i] = arr[num];
+			}
+		}
+	}else{
+		sentence.push(arr[num]);
+	}
+}
+
+function sentenceChecker(arr){
+	for (let i = 0; i < arr.length; i++) {
+		if(sentence.includes(arr[i])){
+			return true;
+		}
+	}
+	return false;
+}
+
+function crossWords(num){
+	let items = document.querySelector(`.item${num}`);
+	items.forEach(e => {
+		
+	});
 }
 
 firstButton.onclick = function() {
-	firstWord = nouns[count.count1];
-	speakNow(firstWord);
-	makeSentence(firstWord);
+	speakNow(nouns[count.count1]);
+	makeSentence(nouns, count.count1);
 	countChecker(1);
 }
 
 secondButton.onclick = function() {
-	secondWord = verbs[count.count2];
-	speakNow(secondWord);
-	makeSentence(secondWord);
+	speakNow(verbs[count.count2]);
+	makeSentence(verbs, count.count2);
 	countChecker(2);
 }
 
 thirdButton.onclick = function() {
-	thirWord = adjectives[count.count3];
-	speakNow(thirWord);
-	makeSentence(thirWord);
+	speakNow(adjectives[count.count3]);
+	makeSentence(adjectives, count.count3);
 	countChecker(3);
 }
 
 fourthButton.onclick = function() {
-	fourthWord = noun[count.count4];
-	speakNow(fourthWord);
-	makeSentence(fourthWord);
+	speakNow(noun[count.count4]);
+	makeSentence(noun, count.count4);
 	countChecker(4);
 }
 
 fifthButton.onclick = function() {
-	fifthWord = settings[count.count5];
-	speakNow(fifthWord);
-	makeSentence(fifthWord);
+	speakNow(settings[count.count5]);
+	makeSentence(settings, count.count5);
 	countChecker(5);
 }
 
 sen.onclick = function() {
 	speakNow(sentence);
+}
+
+story.onclick = function(){
+	var wholeStory = [];
+	buttonList.forEach(e => {
+		 wholeStory.push(e[Math.floor(Math.random() * e.length)]);
+	});
+	speakNow(wholeStory);
+}
+
+reset.onclick = function(){
+	count = {
+		count1: 0,
+		count2: 0,
+		count3: 0,
+		count4: 0,
+		count5: 0
+	};
+	sentence.length = 0;
 }
